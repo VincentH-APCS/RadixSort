096de46c23daf575e2a549dbda55147392acf380
VincentH-APCS <vhuang10@stuy.edu>
2021-01-12 13:04:13 -0500

    Unsure if the methods should be constant time or linear, so assuming linear, wrote the code for the length method

--- Radix.java
+++ Radix.java
@@ -5,11 +5,16 @@ public class Radix{
   }
 
   public static int length(int n){
-
+    int digit = 0;
+    while(n >= 10){
+      digit = digit + 1;
+      n = n / 10;
+    }
+    return digit;
   }
 
   public static void merge(MyLinkedList original, MyLinkedList[] buckets){
 
   }
-  
+
 }
