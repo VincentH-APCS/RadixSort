350497923c34855e7b0644ffae3c657babb4a665
VincentH-APCS <vhuang10@stuy.edu>
2021-01-12 23:01:23 -0500

    Started a basic skeleton of radixSortSimple, unsure if it's correct so far though

--- Radix.java
+++ Radix.java
@@ -24,10 +24,34 @@ public class Radix{
     for(int i = 0; i < buckets.length; i++){
       original.extend(buckets[i]);
     }*/
-  public static void merge(SortableLinkedList original, SortableMyLinkedList[] buckets){
+  public static void merge(SortableLinkedList original, SortableLinkedList[] buckets){
       for(int i = 0; i < buckets.length; i++){
         original.extend(buckets[i]);
       }
+  }
+  public static void radixSortSimple(SortableLinkedList data){
+    SortableLinkedList Zeroes = new SortableLinkedList(); SortableLinkedList Ones = new SortableLinkedList();
+    SortableLinkedList Twos = new SortableLinkedList();  SortableLinkedList Threes = new SortableLinkedList();
+    SortableLinkedList Fours = new SortableLinkedList(); SortableLinkedList Fives = new SortableLinkedList();
+    SortableLinkedList Sixes = new SortableLinkedList(); SortableLinkedList Sevens = new SortableLinkedList();
+    SortableLinkedList Eights = new SortableLinkedList(); SortableLinkedList Nines = new SortableLinkedList();
+    SortableLinkedList[] Bucket = {Zeroes, Ones, Twos, Threes, Fours, Fives, Sixes, Sevens, Eights, Nines};
+    int longest = 0;
+    int = thing = 0;
+    for(int i = 0; i < data.length; i++){
+      if(length(data[i] > longest){
+        longest = length(data.get(i));
+      }
+    }
+    for(int i = 0; i < longest; i++){
+      merge(data, Bucket);
+      for(int j = 0; j < data.length; j++){
+        thing = nth(data[i], i);
+        Bucket[thing].add(data[i]);
+      }
+    }
+
+
   }
 
 }
