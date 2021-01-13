469b9ff2d736bf633e7c6c86f641c959cabf2269
VincentH-APCS <vhuang10@stuy.edu>
2021-01-12 22:24:59 -0500

    Wrote the Merge Method and tested it

--- Radix.java
+++ Radix.java
@@ -21,6 +21,9 @@ public class Radix{
   }
 
   public static void merge(MyLinkedList original, MyLinkedList[] buckets){
+    for(int i = 0; i < buckets.length; i++){
+      original.extend(buckets[i]);
+    }
 
   }
 
